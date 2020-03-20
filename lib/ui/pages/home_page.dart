727dc4f521dac0401b4bb3ea504466218b436cbb
robsonsilv4 <robsonsilv410@gmail.com>
2020-03-20 10:37:04 -0300

    add: information cards

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -6,15 +6,96 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Center(
-        child: CountryCodePicker(
-          onChanged: (county) => print(county.name),
-          initialSelection: 'BR',
-          showCountryOnly: true,
-          showOnlyCountryWhenClosed: true,
-          alignLeft: false,
-        ),
+          child: Column(
+        // mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 15.0,
+              bottom: 10.0,
+            ),
+            child: Text(
+              'COVID-19 Estatísticas',
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          CountryCodePicker(
+            onChanged: (county) => print(county.name),
+            initialSelection: 'BR',
+            showCountryOnly: true,
+            showOnlyCountryWhenClosed: true,
+            alignLeft: false,
+            textStyle: TextStyle(
+              fontSize: 16.0,
+              // fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text('Última atualização: 20-03-2020'),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                _card(
+                  color: Colors.blue,
+                  number: '5.000',
+                  text: 'Confirmados',
+                ),
+                _card(
+                  color: Colors.red,
+                  number: '1.000',
+                  text: 'Mortes',
+                ),
+                _card(
+                  color: Colors.green,
+                  number: '4.000',
+                  text: 'Recuperados',
+                ),
+              ],
+            ),
+          )
+        ],
       )),
+    );
+  }
+
+  Card _card({Color color, String number, String text, double padding}) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      color: color ?? Colors.white,
+      child: Padding(
+        padding: padding ??
+            EdgeInsets.symmetric(
+              horizontal: 15.0,
+              vertical: 10.0,
+            ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              number,
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            Text(
+              text,
+              style: TextStyle(
+                fontSize: 14.0,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
