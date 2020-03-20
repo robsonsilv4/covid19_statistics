de830ff4b43be824db1c464a1d52c18ce334ae57
robsonsilv4 <robsonsilv410@gmail.com>
2020-03-20 00:47:02 -0300

    add: country selection

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,27 @@
+import 'package:country_code_picker/country_localizations.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'ui/pages/home_page.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(App());
 
-class MyApp extends StatelessWidget {
+class App extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      supportedLocales: [
+        Locale('en'),
+        Locale('pt'),
+        Locale('en'),
+      ],
+      localizationsDelegates: [
+        CountryLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
       title: 'COVID-19 Estatísticas',
       theme: ThemeData(
         primarySwatch: Colors.blue,
--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,8 +1,20 @@
+import 'package:country_code_picker/country_code_picker.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+          child: Center(
+        child: CountryCodePicker(
+          onChanged: (county) => print(county.name),
+          initialSelection: 'BR',
+          showCountryOnly: true,
+          showOnlyCountryWhenClosed: true,
+          alignLeft: false,
+        ),
+      )),
+    );
   }
 }
